6c92613669d5f2b6b3709b0573d55d69145ec2c8
Alex87yu <635965101@qq.com>
2020-03-12 10:36:05 +0800

    shuzu lenth cannot be used in func, cal len outof the fuc

---
+++ shuzu.c
@@ -0,0 +1,27 @@
+//#include <linux/module.h>
+#include <stdio.h>
+
+// 一个整型数组的指针，长度为 len， 得到数组中的最小值和最大值 ———— 从外部传入两个指针，从而将所求的两个结果保存带出来，得到多个返回值。
+void minMax(int a[], int len, int *min, int *max) {
+    int i;
+    
+    *min = *max = a[0]; //假定最大值 最小值相等 为a[0]  
+    
+    for(i= 1; i < len; i++) {
+        if( a[i] < *min) {
+          *min = a[i];
+        }
+        
+        if(a[i] > *max) {
+          *max = a[i];
+        }
+    }
+
+}
+int main(){
+int a[] = {1, 2, 3, 4, 5, 7, 8, 9, 15, 18, 25, 33};
+int min, max;
+int len = sizeof(a)/sizeof(a[0]);
+minMax(a, len, &min , &max );
+printf( "min = %d, max = %d \n",  min,  max);
+}
