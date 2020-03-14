7d65dcd2673459759f676979d3f53580bc1f67c8
Alex87yu <635965101@qq.com>
2020-03-14 21:48:28 +0800

    valist and malloc

---
+++ malloc.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+//    p=(char*)malloc(100*sizeof(char));
+
+int main(){
+    char a[20];
+    strcpy(a,"chenyi is bendan");
+    
+    p=(char*)malloc(100*sizeof(char));
+    strcpy(p,"chaoji chaoji ben oooooooo,danshi tade nver hen cong ming!!");
+    printf("%s\n",a);
+    printf("%s\n",p);
+
+
+
+
+    return 0;
+}
